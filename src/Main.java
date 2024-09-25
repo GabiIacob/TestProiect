9cff6c9e573bb81f41d4184845f03a50238ad52b
GabiIacob <gabiiacob1111@gmail.com>
2024-09-25 19:37:40 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main{
     public static void main(String [] args){
         System.out.println("Commit1");
+        int k=5;
+        System.out.println("k are valoarea "+ k);
     }
 }
